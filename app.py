fc8158cfddb71bb7fdcb2e81957079fccb129d65
21F1002963 <21f1002963@ds.study.iitm.ac.in>
2025-07-12 18:39:09 +0530

    Remove Flask-Minify to fix Python 3.13 compatibility issues

--- app.py
+++ app.py
@@ -2,7 +2,6 @@
 import os
 import sys
 from flask_migrate import Migrate
-from flask_minify import Minify
 
 from config import config_dict
 from __init__ import create_app, db
@@ -21,12 +20,8 @@ except KeyError:
 app = create_app(app_config)
 Migrate(app, db)
 
-if not DEBUG:
-    Minify(app=app, html=True, js=False, cssless=False)
-
 if DEBUG:
     app.logger.info('DEBUG            = ' + str(DEBUG)             )
-    app.logger.info('Page Compression = ' + 'FALSE' if DEBUG else 'TRUE' )
     app.logger.info('DBMS             = ' + app_config.SQLALCHEMY_DATABASE_URI)
     app.logger.info('ASSETS_ROOT      = ' + app_config.ASSETS_ROOT )
 
